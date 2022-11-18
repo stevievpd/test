404226d5d97a5735df31b9ccc4afebd183e80f65
steve <stevie.vpd@gmail.com>
2022-11-18 14:48:29 +0800

    Merge conflict done

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
 import '../providers/cart.dart';
 import '../providers/scroll_controller.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
-
-  // ProductItem(this.id, this.title, this.imageUrl);
-
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
@@ -21,12 +14,7 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.id,
-            );
-          },
+          onTap: () {},
           child: Consumer<Product>(
             builder: (ctx, product, _) => Card(
               elevation: 10,
